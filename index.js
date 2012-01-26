0a9eb7aae9ad57ecfcfb18da2783b8beb1a9033c
Morteza Milani <mrtz.milani@googlemail.com>
2012-01-26 10:06:29 +0330

    Version 1.0.0

--- index.js
+++ index.js
@@ -1,18 +1,92 @@
-var express = require('express'),
-    nowjs   = require('now');
+var express  = require('express'),
+    request  = require('request'),
+    db       = require('./lib/db'),
+    nowjs    = require('now');
 
-var log = console.log;
 var server = express.createServer();
+var everyone = nowjs.initialize(server);
 
 server.use(server.router);
 server.use(express.static(__dirname + '/public'));
 
-var everyone = nowjs.initialize(server);
+//db.create({msg:'hey',link:'http://google.com',title:'title',desc:'description'},function(err){console.log(err)});
 
-everyone.now.logStuff = function(msg){
-  log(msg);
+everyone.now.load = function(cursor,callback) {
+  var length = 20,
+      self   = this;
+
+  db.fetch(cursor*length,length,function(err,items){
+
+    if(err){
+      return callback(err.toString());
+    }
+
+    self.now.onItem(items);
+  });
+
+}
+
+function isUrl(str){
+
+  var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  return regexp.test(str);
+
+}
+
+everyone.now.share = function(data,cb){
+  //TODO validate data.link;
+  var self = this;
+  
+  if(!isUrl(data.link)){
+    return cb("Provided Link is not valid.");
+  }
+
+  request(data.link, function(err,resp,body){
+
+    if( err || resp.statusCode != 200 ){
+      return cb(err);
+    }
+
+    var title = body.match(/<title>([^<]*)<\/title>/);
+    var desc = body.match(/<meta name="description" content="([^"]*)">/);
+    
+    if(title){
+      data.title = title[1];
+    }else{
+      data.title = resp.request.host;
+    }
+
+    if(desc){
+      data.desc = desc[1];
+    }else{
+      data.desc = '';
+    }
+
+    db.create(data,function(err){
+
+      cb(err)
+
+      everyone.now.onItem([data]);
+
+    });
+
+  });
+
+}
+
+everyone.now.remove = function(id,callback){
+  //TODO validate id
+  db.remove(id,function(err){
+    if(err){
+      callback(err.toString());
+    }else{
+      callback(null);
+    }
+
+    everyone.now.onRemove(id);
+  });
 }
 
 server.listen(9002,function() {
-    log('Ready');
+  console.log('Listening on 9002');
 });
---
+++ public/js/main.js
@@ -0,0 +1,172 @@
+(function(){
+
+  var cursor = 0;
+
+  function initialize(){
+
+    $(window).scroll(function(){
+      var totalHeight = $(document).height() - $(window).height();
+      var scrollTop = $(this).scrollTop() + 10;
+      if( scrollTop >= totalHeight ){
+        loadItems();
+      }
+    });
+
+    $('#share-btn').click(function(){
+       if(window.webkitNotifications && !window.webkitNotifications.checkPermission()) window.webkitNotifications.requestPermission();
+    });
+    $('#remove-modal').modal({backdrop:false,keyboard:false});
+
+    $('#items').delegate('.remove','click',function(){
+      $('#removeId').val($(this).parent().attr('id'));
+      $('#remove-modal').modal();
+    });
+
+    $('#yes-btn').click(function(){
+
+      if($(this).hasClass('disabled')) return;
+
+      $(this).addClass('disabled');
+      $('#no-btn').addClass('disabled');
+
+      removeItem($('#removeId').val(),function(err){
+
+        $('#yes-btn').removeClass('disabled');
+        $('#no-btn').removeClass('disabled');
+
+        if(err){
+          $('#remove-error').show();
+        }else{
+          $('#remove-error').hide();
+          $('#remove-modal').modal();
+        }
+        
+      });
+
+    });
+
+    $('#no-btn').click(function(){
+
+      if($(this).hasClass('disabled')) return;
+
+      $('#remove-error').hide();
+      $('#remove-modal').modal();
+    });
+
+    $('#save-btn').click(function(){
+    
+      if($(this).hasClass('disabled')) return;
+
+      $('#save-btn').addClass('disabled');
+      $('#cancel-btn').addClass('disabled');
+        
+      $('#share-error').hide();
+
+      var data = {
+        msg: $('#msg').val(),
+        link: $('#link').val()
+      }
+
+      shareItem(data,function(err){
+
+        $('#save-btn').removeClass('disabled');
+        $('#cancel-btn').removeClass('disabled');
+        
+        if(err){
+          $('#share-error').show();
+        }else{
+          $('#share-error').hide();
+          $('#modal').modal();
+        }
+
+      });
+
+    });
+
+    $('#cancel-btn').click(function(){
+      
+      if($(this).hasClass('disabled')) return;
+
+      $('#share-error').hide();
+      $('#modal').modal();
+    });
+
+  }
+
+  function itemsLoaded(items){
+    var template = '<div id="%_id%" class="item">\
+                      <a href="#" class="remove">x</a>\
+                      <div class="msg">%msg%</div>\
+                      <div class="title">\
+                        <a href="%link%">%title%</a>\
+                      </div>\
+                      <div class="desc">%desc%</div>\
+                    </div>';
+    
+    $('.loading').remove();
+    var temp;
+    items.forEach(function(item){
+      temp = template.replace(/%(.{2,5})%/g,function(substr,index){
+        return item[index];
+      });
+      $('#items').prepend(temp);
+    });
+
+    if(items.length > 0){
+      notifyUser(items[0],items.length);
+    }
+  }
+
+  function notifyUser(item,count){
+    if (window.webkitNotifications && window.webkitNotifications.checkPermission() == 0) {
+      var notification = window.webkitNotifications.createNotification(null,item.title,item.desc);
+      notification.show();
+      if(count >= 1){
+        notification = window.webkitNotifications.createNotification(null,'More links shared...',--count +' more items loaded');
+        notification.show();
+      }
+    }
+  }
+
+  function itemRemoved(id){
+    $('#'+id).hide().remove();
+  }
+
+  function shareItem(data,callback){
+    now.share(data,function(err){
+      callback(err);
+    });
+  }
+
+  function removeItem(id,callback){
+    now.remove(id,function(err){
+      callback(err);
+    });
+  }
+
+  function loadItems(){
+    $('#items').after('<div class="loading"/>');
+    $('.load-error').remove();
+    now.load(cursor++,function(err){
+      if(err){
+        $('#items').append('<div class="load-error">Error loading shared items. Trying in 10 seconds...</div>');
+        $('.loading').remove();
+        cursor--;
+        setTimeout(loadItems,10000);
+      }
+    });
+  }
+
+  $(document).ready(function(){
+
+    initialize();
+    now.onItem = itemsLoaded;
+    now.onRemove = itemRemoved;
+    
+    now.ready(function(){
+      loadItems();
+    });
+
+  });
+
+})();
